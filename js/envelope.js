a45572c997492fe1855dcaf8a239ebfda47d8a48
Dmytro Melnyk <umfhardy2666@gmail.com>
2024-07-17 17:50:33 +0300

    updated code

--- js/envelope.js
+++ js/envelope.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const envelope = document.getElementById('floating-envelope');
-    let velocity = { x: 2, y: 2 }; // Початкова швидкість
+    let velocity = { x: 2, y: 2 };
     let position = { 
-        x: window.innerWidth - 200, // Повна ширина вікна мінус ширина конверта
-        y: window.innerHeight - 200 // Висота вікна мінус висота конверта
+        x: window.innerWidth - 200,
+        y: window.innerHeight - 200
      };
     let isScrolling;
 
@@ -11,19 +11,16 @@ document.addEventListener('DOMContentLoaded', () => {
         const maxX = window.innerWidth - envelope.clientWidth;
         const maxY = window.innerHeight - envelope.clientHeight;
 
-        // Оновлюємо позицію
         position.x += velocity.x;
         position.y += velocity.y;
 
-        // Перевіряємо зіткнення з краями
         if (position.x <= 0 || position.x >= maxX) {
-            velocity.x *= -1; // Змінюємо напрямок по X
+            velocity.x *= -1;
         }
         if (position.y <= 0 || position.y >= maxY) {
-            velocity.y *= -1; // Змінюємо напрямок по Y
+            velocity.y *= -1;
         }
 
-        // Застосовуємо нову позицію
         envelope.style.left = `${position.x}px`;
         envelope.style.top = `${position.y}px`;
     }
@@ -31,12 +28,30 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', () => {
         window.clearTimeout(isScrolling);
         
-        // Викликаємо функцію руху
         moveEnvelope();
 
-        // Встановлюємо таймер на зупинку
         isScrolling = setTimeout(() => {
-            // Зупиняємо рух, коли скрол зупиняється
         }, 100);
     });
 });
+
+window.addEventListener("load", function () {
+    setTimeout(hideLoader, 1500);
+  });
+  
+  window.addEventListener("DOMContentLoaded", function () {
+    document.querySelector('.overlay').style.display = 'block';
+  });
+  
+  function hideLoader() {
+    const loader = document.querySelector(".loader-wrapper");
+    const overlay = document.querySelector(".overlay");
+  
+    loader.classList.add("hidden");
+    overlay.classList.add("hidden");
+  
+    loader.addEventListener('animationend', () => {
+      loader.remove();
+      overlay.remove();
+    });
+  }
